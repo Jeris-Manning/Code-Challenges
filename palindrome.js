c31eadae4a64b2d08e7d5ed6302dadcbb96aad8d
wickedgoose moose <the.goose.moose@gmail.com>
2020-06-03 02:05:29 -0600

    adds 2 city schedule

---
+++ palindrome.js
@@ -0,0 +1,8 @@
+// Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
+
+// param {number}
+// return {boolean}
+
+var isPalindrome = function(x) {
+
+};
